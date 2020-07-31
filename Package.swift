ce9e171cae1a9e55fedca8f58b719184225c8415
Helge Heß <devteam@zeezide.de>
2020-07-31 19:01:58 +0200

    First attempt to conditionally include MacroApp (doesn't work)
    ...

--- Package.swift
+++ Package.swift
@@ -2,6 +2,40 @@
 
 import PackageDescription
 
+let dependencies : [ PackageDescription.Package.Dependency ] = [
+  .package(url: "https://github.com/Macro-swift/Macro.git",
+           from: "0.5.4"),
+  .package(url: "https://github.com/Macro-swift/MacroExpress.git",
+           from: "0.5.3"),
+  .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git",
+           .upToNextMajor(from:"0.2.0"))
+]
+
+let platforms         : [ Platform           ]
+let extraDependencies : [ PackageDescription.Package.Dependency ]
+let extraTargetDeps   : [ PackageDescription.Target.Dependency  ]
+if #available(macOS 10.15, iOS 13, *) {
+  platforms = [ .macOS(.v10_15) ]
+  extraDependencies = [
+    .package(url: "https://github.com/Macro-swift/MacroApp.git", from: "0.5.0")
+  ]
+  extraTargetDeps = [ "MacroApp" ]
+}
+else {
+  platforms = [ .macOS(.v10_13) ] // drop once #152 is fixed in the runtime
+  extraDependencies = []
+  extraTargetDeps   = []
+}
+
+let targets : [ PackageDescription.Target ] = [
+  .target(name: "MacroLambdaCore",
+          dependencies: [ "MacroCore", "http",
+                          "AWSLambdaRuntime", "AWSLambdaEvents" ]),
+  .target(name: "MacroLambda",
+          dependencies: [ "MacroLambdaCore", "AWSLambdaRuntime",
+                          "MacroExpress" ] + extraTargetDeps)
+]
+
 let package = Package(
   
   name: "MacroLambda",
@@ -13,21 +47,6 @@ let package = Package(
     .library(name: "MacroLambda",     targets: [ "MacroLambda"     ])
   ],
   
-  dependencies: [
-    .package(url: "https://github.com/Macro-swift/Macro.git",
-             from: "0.5.4"),
-    .package(url: "https://github.com/Macro-swift/MacroExpress.git",
-             from: "0.5.3"),
-    .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git",
-             .upToNextMajor(from:"0.2.0"))
-  ],
-  
-  targets: [
-    .target(name: "MacroLambdaCore", 
-            dependencies: [ "MacroCore", "http",
-                            "AWSLambdaRuntime", "AWSLambdaEvents" ]),
-    .target(name: "MacroLambda",     
-            dependencies: [ "MacroLambdaCore", "AWSLambdaRuntime",
-                            "MacroExpress" ])
-  ]
+  dependencies : dependencies + extraDependencies,
+  targets      : targets
 )
