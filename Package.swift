94a44bcc343cf1cebdad5ec8e66867c5487be7e5
Helge Heß <devteam@zeezide.de>
2020-08-08 15:48:43 +0200

    Properly use single value headers
    Was sending multi, even when there was only a single
    value. Which made e.g. content-type being dropped?

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     .package(url: "https://github.com/Macro-swift/Macro.git",
              from: "0.5.5"),
     .package(url: "https://github.com/Macro-swift/MacroExpress.git",
-             from: "0.5.4"),
+             from: "0.5.5"),
     .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git",
              .upToNextMajor(from:"0.2.0"))
   ],
