444d3fee1103e4d92ac8adc89b500521c56e6f34
Helge Heß <devteam@zeezide.de>
2020-12-05 15:38:22 +0100

    Bump Macro requirement to 0.5.7
    ... API change in `console`.

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
   
   dependencies: [
     .package(url: "https://github.com/Macro-swift/Macro.git",
-             from: "0.5.5"),
+             from: "0.5.7"),
     .package(url: "https://github.com/Macro-swift/MacroExpress.git",
-             from: "0.5.5"),
+             from: "0.5.7"),
     .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git",
              .upToNextMajor(from:"0.2.0"))
   ],
